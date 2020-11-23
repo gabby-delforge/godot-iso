339ab1d168d52fc55f3c63a8ba727fda826671f2
Alex Ye <alexander.ye7@gmail.com>
2020-11-22 20:50:57 -0800

    reorganized troll movement code -- still buggy

--- troll.gd
+++ troll.gd
@@ -18,14 +18,12 @@ func _ready():
 func _input(ev):
 	if ev and ev is InputEventMouseButton and ev.button_index == BUTTON_RIGHT and ev.is_pressed() and is_selected:
 		target = get_global_mouse_position()
-		velocity = position.direction_to(target) * speed
 	
 func _physics_process(_delta):
 	if position.distance_to(target) < 5:
 		velocity = Vector2.ZERO
 	else:
-		look_at(target)
-		velocity = move_and_slide(velocity)
+		move_to(target)
 #	var motion = Vector2()
 #	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 #	motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
@@ -47,5 +45,10 @@ func on_select():
 func on_deselect():
 	is_selected = false
 	sprite.set_material(null)
+
+func move_to(target):
+	velocity = position.direction_to(target) * speed
+	# look_at(target)
+	velocity = move_and_slide(velocity)
 
 	
