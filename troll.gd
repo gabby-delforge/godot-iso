526b58adf3ede77965e2caa76c7ca5df2e65ab7f
gabrielle-delforge <gabrielle.delforge@berkeley.edu>
2020-11-22 17:05:57 -0800

    basic rts camera

--- troll.gd
+++ troll.gd
@@ -3,13 +3,14 @@ extends KinematicBody2D
 const MOTION_SPEED = 160 # Pixels/second.
 
 func _physics_process(_delta):
-	var motion = Vector2()
-	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	motion.y /= 2
-	motion = motion.normalized() * MOTION_SPEED
-	#warning-ignore:return_value_discarded
-	move_and_slide(motion)
+#	var motion = Vector2()
+#	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+#	motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+#	motion.y /= 2
+#	motion = motion.normalized() * MOTION_SPEED
+#	#warning-ignore:return_value_discarded
+#	move_and_slide(motion)
+	pass
 
 func _input(ev):
 	if ev is InputEventKey and ev.scancode == KEY_K:
